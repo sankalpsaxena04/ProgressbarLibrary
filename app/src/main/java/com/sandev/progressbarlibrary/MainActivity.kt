7ba9088a4c7bb3c47f2e535154ffa125e91b6ab3
SANKALP SAXENA <sankalpsaxena2004@gmail.com>
2025-06-25 12:03:47 +0530

    updated

--- app/src/main/java/com/sandev/progressbarlibrary/MainActivity.kt
+++ app/src/main/java/com/sandev/progressbarlibrary/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sandev.circular_progressbar.CircularProgressbar
 import com.sandev.progressbarlibrary.ui.theme.ProgressbarLibraryTheme
 
@@ -21,7 +24,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProgressbarLibraryTheme {
 
-                CircularProgressbar()
+                CircularProgressbar(
+                    Modifier.size(300.dp),
+                    cpbarThickness = 24.dp,
+                    title="Testing",
+                    titleVisibility = true,
+                    completedPercentage = 75.0
+                )
             }
         }
     }
--- circular_progressbar/src/main/java/com/sandev/circular_progressbar/CircularProgressbar.kt
+++ circular_progressbar/src/main/java/com/sandev/circular_progressbar/CircularProgressbar.kt
@@ -7,14 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -27,22 +27,24 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun CircularProgressbar(modifier: Modifier,
-    completedPercentage:Double=30.0,
-    backgroundColor: Color= Color(0,0,0,0),
-    progressbarGradientColors:List<Color> = listOf(Color(0,0,255), Color(0,255,0),Color(0,0,255)),
-    textVisibility: Boolean=true
-){
-
+fun CircularProgressbar(
+    modifier: Modifier = Modifier.size(200.dp),
+    cpbarThickness:Dp=20.dp,
+    completedPercentage: Double = 30.0,
+    backgroundColor: Color = Color.Transparent,
+    progressbarGradientColors: List<Color> = listOf(Color(0, 0, 255), Color(0, 255, 0), Color(0, 0, 255)),
+    percentageVisibility: Boolean = true,
+    title: String,
+    titleVisibility: Boolean = false
+) {
     var targetAngle by remember { mutableFloatStateOf(0f) }
-    val isTextVisible  by remember { mutableStateOf(textVisibility) }
 
     LaunchedEffect(Unit) {
-        targetAngle = (completedPercentage*3.6).toFloat()
+        targetAngle = (completedPercentage * 3.6).toFloat()
     }
 
     val animatedSweepAngle by animateFloatAsState(
@@ -51,33 +53,49 @@ fun CircularProgressbar(modifier: Modifier,
         label = "ArcAnimation"
     )
 
-    Box(
-        modifier = modifier
-            .size(200.dp)
-            .background(backgroundColor)
-            .padding(16.dp)
+    Column(
+        modifier = Modifier.wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-    Canvas(modifier = Modifier.fillMaxSize()){
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-        val arcSize = Size(canvasWidth, canvasHeight)
+        Box(
+            modifier = modifier
+                .background(backgroundColor)
+                .padding(cpbarThickness/2),
+            contentAlignment = Alignment.Center
+        ) {
+            Canvas(modifier = Modifier.matchParentSize()) {
+                val canvasWidth = size.width
+                val canvasHeight = size.height
+                val arcSize = Size(canvasWidth, canvasHeight)
 
-        drawArc(
-            brush = Brush.sweepGradient(
-                colors = progressbarGradientColors,
-                center = Offset(canvasWidth / 2, canvasHeight / 2)
-            ),
-            startAngle = -90F,
-            sweepAngle = animatedSweepAngle,
-            useCenter = false,
-            topLeft = Offset(0f,0f),
-            size = arcSize,
-            style = Stroke(width = 20f, cap = StrokeCap.Round, join = StrokeJoin.Round)
+                drawArc(
+                    brush = Brush.sweepGradient(
+                        colors = progressbarGradientColors,
+                        center = Offset(canvasWidth / 2, canvasHeight / 2)
+                    ),
+                    startAngle = -90F,
+                    sweepAngle = animatedSweepAngle,
+                    useCenter = false,
+                    topLeft = Offset(0f, 0f),
+                    size = arcSize,
+                    style = Stroke(width = cpbarThickness.toPx(), cap = StrokeCap.Round, join = StrokeJoin.Round)
+                )
+            }
+            if (percentageVisibility) {
+                Text(
+                    text = "${completedPercentage.toInt()}%",
+                    fontSize = 36.sp
+                )
+            }
+        }
+
+        if (titleVisibility) {
+            Text(
+                text = title,
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(top = 10.dp)
             )
-
-    }
-        if(isTextVisible){
-            Text(modifier = Modifier.align(Alignment.Center), text = "$completedPercentage%", fontSize = 50.sp)
         }
     }
 }
